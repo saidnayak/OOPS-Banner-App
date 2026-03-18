67f7d115298da343a1d7622b533e3a02e418403e
Sai Dattatreya Nayak <saidattatreyanayak08@gmail.com>
2026-03-18 15:37:00 +0530

    UC5: Inline array initialization with String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,34 +2,32 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Step 1: Create array
-        String[] banner = new String[7];
+        // Inline array initialization
+        String[] banner = {
 
-        // Step 2: Fill array using String.join()
+            String.join(" ",
+                    "  *****", "   ", "*****", "   ", "******", "   ", "*****"),
 
-        banner[0] = String.join(" ",
-                "  *****", "   ", "*****", "   ", "******", "   ", "*****");
+            String.join(" ",
+                    " *     *", " ", "*     *", " ", "*     *", " ", "*"),
 
-        banner[1] = String.join(" ",
-                " *     *", " ", "*     *", " ", "*     *", " ", "*");
+            String.join(" ",
+                    " *     *", " ", "*     *", " ", "*     *", " ", "*"),
 
-        banner[2] = String.join(" ",
-                " *     *", " ", "*     *", " ", "*     *", " ", "*");
+            String.join(" ",
+                    " *     *", " ", "*     *", " ", "******", " ", "****"),
 
-        banner[3] = String.join(" ",
-                " *     *", " ", "*     *", " ", "******", " ", "****");
+            String.join(" ",
+                    " *     *", " ", "*     *", " ", "*", " ", "      *"),
 
-        banner[4] = String.join(" ",
-                " *     *", " ", "*     *", " ", "*", " ", "      *");
+            String.join(" ",
+                    " *     *", " ", "*     *", " ", "*", " ", "      *"),
 
-        banner[5] = String.join(" ",
-                " *     *", " ", "*     *", " ", "*", " ", "      *");
+            String.join(" ",
+                    "  *****", "   ", "*****", "   ", "*", "   ", "*****")
+        };
 
-        banner[6] = String.join(" ",
-                "  *****", "   ", "*****", "   ", "*", "   ", "*****");
-
-
-        // Step 3: Print using loop
+        // Loop to print
         for (String line : banner) {
             System.out.println(line);
         }

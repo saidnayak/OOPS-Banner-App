c161c76b1f8c286ca4a35688be37ac2ac0006bb3
Sai Dattatreya Nayak <saidattatreyanayak08@gmail.com>
2026-03-18 13:36:39 +0530

    Coded UC1 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPS Banner App
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}

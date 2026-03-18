98adde9fbab05db902ba246da7bf41277de12fcb
Sai Dattatreya Nayak <saidattatreyanayak08@gmail.com>
2026-03-18 20:17:18 +0530

    UC6: Refactored using static methods for O, P, S patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,34 +2,63 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Inline array initialization
-        String[] banner = {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
-            String.join(" ",
-                    "  *****", "   ", "*****", "   ", "******", "   ", "*****"),
+        String[] banner = new String[7];
 
-            String.join(" ",
-                    " *     *", " ", "*     *", " ", "*     *", " ", "*"),
+        // Combine O O P S
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ",
+                    oPattern[i], oPattern[i], pPattern[i], sPattern[i]);
+        }
 
-            String.join(" ",
-                    " *     *", " ", "*     *", " ", "*     *", " ", "*"),
-
-            String.join(" ",
-                    " *     *", " ", "*     *", " ", "******", " ", "****"),
-
-            String.join(" ",
-                    " *     *", " ", "*     *", " ", "*", " ", "      *"),
-
-            String.join(" ",
-                    " *     *", " ", "*     *", " ", "*", " ", "      *"),
-
-            String.join(" ",
-                    "  *****", "   ", "*****", "   ", "*", "   ", "*****")
-        };
-
-        // Loop to print
+        // Print banner
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // O pattern
+    public static String[] getOPattern() {
+        return new String[]{
+
+                String.join(" ", "  *****  "),
+                String.join(" ", " *     * "),
+                String.join(" ", " *     * "),
+                String.join(" ", " *     * "),
+                String.join(" ", " *     * "),
+                String.join(" ", " *     * "),
+                String.join(" ", "  *****  ")
+        };
+    }
+
+    // P pattern
+    public static String[] getPPattern() {
+        return new String[]{
+
+                String.join(" ", " ******  "),
+                String.join(" ", " *     * "),
+                String.join(" ", " *     * "),
+                String.join(" ", " ******  "),
+                String.join(" ", " *       "),
+                String.join(" ", " *       "),
+                String.join(" ", " *       ")
+        };
+    }
+
+    // S pattern
+    public static String[] getSPattern() {
+        return new String[]{
+
+                String.join(" ", "  *****  "),
+                String.join(" ", " *       "),
+                String.join(" ", " *       "),
+                String.join(" ", "  *****  "),
+                String.join(" ", "       * "),
+                String.join(" ", "       * "),
+                String.join(" ", "  *****  ")
+        };
     }
 }
